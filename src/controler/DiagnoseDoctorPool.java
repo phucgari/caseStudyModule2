2d249d9d3bbfb6195baf8fb556ea02a99fb090e2
phucgari <phucgari@gmail.com>
2023-02-13 22:47:03 +0700

    add smthg

--- src/controler/DiagnoseDoctorPool.java
+++ src/controler/DiagnoseDoctorPool.java
@@ -1,4 +1,13 @@
 package controler;
+
+import model.doctor.DiagnoseDoctor;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 // design pattern Object pool
 public class DiagnoseDoctorPool {
+    private static final int NUMBER_OF_DIAGNOSE_DOCTOR=4;
+    private final Queue<DiagnoseDoctor> available= new LinkedList<>();
+    private final Queue<DiagnoseDoctor> inuse= new LinkedList<>();
 }
